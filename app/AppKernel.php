9665b0cbedfbde4ad3314634f1ffef8145158218
n.djabon <neph@cineca.it>
2015-02-27 17:24:09 +0100

    new bundle in kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Ennva\WebsiteBundle\EnnvaWebsiteBundle()
+            new Ennva\WebsiteBundle\EnnvaWebsiteBundle(),
+            new Ennva\EcommerceBundle\EnnvaEcommerceBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
